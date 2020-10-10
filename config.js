f0e7c1dd6ed07643d933519636d436e0315d9e3c
SHEN <>
2020-10-10 13:57:06 +0800

    add

---
+++ config.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: SHJ
+ * @Date: 2018-06-22 14:18:19
+ * @Last Modified by: SHEN
+ * @Last Modified time: 2020-09-28 12:01:33
+ */
+window.LOCAL_CONFIG = {
+  // API_HOME: 'http://47.92.98.185:7300/mock/5e5731f7690d223ef6e88247/example'
+  API_HOME: 'http://127.0.0.1:11000/mock/5e5731f7690d223ef6e88247/example'
+}
